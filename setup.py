28716ceb785964a1e4b506d447783155f2721eca
Luis2501 <lsgm16308@gmail.com>
2022-07-27 09:37:48 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
 	author="Luis E. Sánchez Glz.",
 	author_email="lsgm16308@gmail.com"
 	url="https://luis2501.github.io/portfolio/portfolio-2/"
-	packages=["physicspy", "physicspy/physics", "physicspy/integration", "physics/others"]
+	packages=["physicspy", "physicspy/physics", "physicspy/integration", "physicspy/others"]
 	install_requires=["numpy", "matplotlib", "plotly"]
 	
 )
